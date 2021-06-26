4dd2081a1f16da461e3f423ddf91dea3d54f346f
Raupov <m.raupovios6766@gmail.com>
2021-06-26 18:21:25 +0500

    add back color

--- Lesson_77_Git/MainVC.swift
+++ Lesson_77_Git/MainVC.swift
@@ -11,7 +11,7 @@ class MainVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        view.backgroundColor = .red
     }
 
     @IBAction func btnPressed(_ sender: Any) {
